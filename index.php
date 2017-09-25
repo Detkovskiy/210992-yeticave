12862387739d35da840067eadbc15e9046579752
Yura Detkovskiy <9119113142@mail.ru>
2017-09-25 14:41:46 +0300

    пагинация

--- index.php
+++ index.php
@@ -6,17 +6,31 @@ $title = "Главная";
 require_once 'config.php';
 require_once 'init.php';
 
-$sql_lots = '
-    SELECT l.id, lot_name, cost, image, category_name, data_end 
-    FROM lots l 
-    JOIN categories c ON category_id = c.id;';
+$current_page = $_GET['page'] ?? 1;
+$page_item = 3;
 
-$array_lots = select_data($link, $sql_lots, '');
+$sql_count_lots = 'SELECT count(*) as count FROM lots;';
+$count_lots = select_data($link, $sql_count_lots, '')[0];
+
+$page_count = ceil($count_lots['count'] / $page_item);
+$offset = ($current_page - 1) * $page_item;
+$pages = range(1, $page_count);
+
+$sql_lots =
+    'SELECT l.id, lot_name, cost, image, category_name, data_end
+    FROM lots l
+    JOIN categories c ON category_id = c.id
+    LIMIT ? OFFSET ?;';
+
+$array_lots = select_data($link, $sql_lots, [$page_item, $offset]);
 
 $content = render_template('templates/index.php',
     [
         'array_lots' => $array_lots,
-        'categories' => $categories
+        'categories' => $categories,
+        'pages' => $pages,
+        'page_count' => $page_count,
+        'current_page' => $current_page
     ]);
 
 $layout = render_template('templates/layout.php',
--- templates/index.php
+++ templates/index.php
@@ -55,3 +55,15 @@
         <?php endforeach; ?>
     </ul>
 </section>
+<?php if ($page_count > 1) : ?>
+<ul class="pagination-list">
+    <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
+    <?php foreach ($pages as $page) : ?>
+        <li class="pagination-item <?= ($page == $current_page) ? 'pagination-item-active' : '' ;?> ">
+            <a href="index.php?page=<?= $page; ?>"><?= $page; ?></a>
+        </li>
+        <?php endforeach; ?>
+
+    <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
+</ul>
+<?php endif; ?>
