1d3871d0573ada84070f8ed0660ae07cc8cd06cd
Yura Detkovskiy <9119113142@mail.ru>
2017-09-29 11:16:59 +0300

    лоты +

--- templates/layout.php
+++ templates/layout.php
@@ -113,7 +113,6 @@
 <script>
     function select_category() {
         var id_cat = document.getElementById("select").value;
-        //alert(x);
         location.href = "http://localhost:8888/210992-yeticave/index.php?id=" + id_cat;
 
     }
